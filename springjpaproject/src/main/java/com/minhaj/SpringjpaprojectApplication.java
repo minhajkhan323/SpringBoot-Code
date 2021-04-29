5c014eb80661372e5ec31fc1103730100d144954
minhajkhan323 <minhajkhan323@gmail.com>
2021-04-29 17:18:37 +0530

    first commit

---
+++ springjpaproject/src/main/java/com/minhaj/SpringjpaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.minhaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaprojectApplication.class, args);
+	}
+
+}
---
+++ springjpaproject/src/test/java/com/minhaj/SpringjpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
